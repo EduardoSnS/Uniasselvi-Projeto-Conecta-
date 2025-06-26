ddbd382ad7ef37601e6ca33b7bcdac5445a8ad86
EduardoSnS <149635297+EduardoSnS@users.noreply.github.com>
2025-06-26 13:11:01 -0300

    Protegendo URL com dotenv e atualizando server.js

--- conecta-plus-backend/server.js
+++ conecta-plus-backend/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/conecta_plus', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB conectado!'))
+.then(() => console.log('💾 MongoDB Atlas conectado!'))
 .catch((err) => console.log('Erro MongoDB:', err));
 
 const SuporteSchema = new mongoose.Schema({
